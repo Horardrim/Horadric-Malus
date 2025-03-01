6963574049d0c1d517676ef32d7e1e494df8ecb7
TalRasha <524948250@qq.com>
2025-03-01 13:26:27 +0800

    array parameter usage

---
+++ array_params/main.cxx
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define ARRAY_SIZE 10
+
+/*
+ * array parameter with size
+ */
+void arrayFuncI(int arr[], int size)
+{
+
+}
+
+/*
+ * reference of an array
+ */
+void arrayFuncII(int (&array)[ARRAY_SIZE])
+{
+
+}
+
+int main(int argc, char ** argv)
+{
+
+}
